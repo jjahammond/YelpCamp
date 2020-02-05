4c1167e0a3ff8d844bc2cc7f358c1a9d40505056
James <jjahammond@hotmail.co.uk>
2020-02-05 23:40:57 +0000

    Fixing PORT v2.1.3

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.get('/*', (req,res) => {
 seedDB();
 
 // *************  Request listener *******************
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log("Server running...")
 });
