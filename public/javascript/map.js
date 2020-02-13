3453b4a2849e3abd6d043b281a4a8c7a6be91438
James <jjahammond@hotmail.co.uk>
2020-02-13 21:49:59 +0000

    Modal updates v2.3.6

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,14 +1,13 @@
 function initMap() {
-  var campgroundLatLng = {lat: 0, lng: 0};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: campgroundLatLng,
+    center: {lat: 0, lng: 0},
     zoom: 14
   });
 
   var geocoder = new google.maps.Geocoder();
   var address = campground.address;
-  // "Mill Lane, Polzeath, Wadebridge PL27 6SS"
+
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       map.setCenter(results[0].geometry.location);
